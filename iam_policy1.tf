4eaa1d271d78e1ddf35c18dc275349ef85787a86
Mule <SFNKDOV@vw.vwgroup.com>
2025-02-28 16:21:42 +0530

    changes in policy

--- iam_policy1.tf
+++ iam_policy1.tf
@@ -1,5 +1,11 @@
 
 
+resource "aws_iam_policy_attachment" "" {
+  name       = "cloudwatch-policy"
+  roles      = [aws_iam_role.iam_role_terraform.name]
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
 resource "aws_iam_policy_attachment" "cloudwatch_policy" {
   name       = "cloudwatch-policy"
   roles      = [aws_iam_role.iam_role_terraform.name]
