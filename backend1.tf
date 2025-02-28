8a3a7a247b0b475b24c9617d4106451e7eb3b6a6
Mule <SFNKDOV@vw.vwgroup.com>
2025-02-28 13:19:03 +0530

    add to new bucket

--- backend1.tf
+++ backend1.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "task-terraform1-backend"
-    key    = "network/terraform.tfstate"
+    bucket = "statefilestores-backend"
+    key    = "info/terraform.tfstate"
     region = "us-east-1"
   }
 }
