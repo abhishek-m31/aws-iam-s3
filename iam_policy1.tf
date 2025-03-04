f1c3103d606420f68a0568ed26622ed41b22b753
Mule <SFNKDOV@vw.vwgroup.com>
2025-03-04 11:45:34 +0530

    add some policies

--- iam_policy1.tf
+++ iam_policy1.tf
@@ -5,10 +5,22 @@ resource "aws_iam_policy_attachment" "cloudwatch_policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
 
+resource "aws_iam_policy_attachment" "permissionsmfa" {
+  name       = "permissionmfa"
+  roles      = [aws_iam_role.iam_role_terraform.name]
+  policy_arn = "arn:aws:iam::761018847268:policy/PermissionsWithMFA"
+}
+
 resource "aws_iam_policy_attachment" "listandread" {
   name       = "listandread"
   roles      = [aws_iam_role.iam_role_terraform.name]
   policy_arn = "arn:aws:iam::761018847268:policy/MyIAMPermisions"
+}
+
+resource "aws_iam_policy_attachment" "timerestrictedaccess" {
+  name       = "timerestrictedaccess"
+  roles      = [aws_iam_role.iam_role_terraform.name]
+  policy_arn = "arn:aws:iam::761018847268:policy/TimeRestrictedAccess"
 }
 
 resource "aws_iam_policy_attachment" "sns_policy" {
